133cf7b7068ba5365cbb2aa042ff6fa597a13bc9
Vadimchik <yuhnevich.work@gmail.com>
2022-06-05 17:04:06 +0300

    Hehe

--- src/lesson/Main.java
+++ src/lesson/Main.java
@@ -1,8 +1,19 @@
 package lesson;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello git");
+
+        Scanner x = new Scanner(System.in);
+        int n = x.nextInt();
+        int k = x.nextInt();
+        long[] arr= new long[n];
+        long sum = 0;
+        for (int i = 0; i < n; i++) {
+            arr[i] = x.nextLong();
+        }
+
     }
 }
